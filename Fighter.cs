44e208aa6e6a952778054b7d70e163f6a1effe1b
Omar Santiago <omarkenosia@gmail.com>
2021-07-11 21:12:11 -0400

    Add files via upload

---
+++ Fighter.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    public float MoveSpeed = 3.5f;
+    public float MinPosition = -4.0f;
+    public float MaxPosition = 4.0f;
+    public string PunchBoneName = "bone_7";
+    public string KickBoneName = "bone_12";
+    private Animator anim = null;
+    private HittingBodyPart punchbone = null;
+    private HittingBodyPart kickbone = null;
+
+    private void Start()
+    {
+        //reference to animator component
+        anim = this.GetComponent<Animator>();
+
+        //reference to scripts on punch and kick bones
+        punchbone = GetBodyPartScript(PunchBoneName);
+        kickbone = GetBodyPartScript(KickBoneName);
+    }
+    private void Update()
+    {
+        //control movement
+        float horz = Input.GetAxis("Horizontal");  //-1 .. 1
+        if (horz > 0 && this.transform.position.x < MaxPosition)
+        {
+            this.transform.Translate(Vector3.right * horz * MoveSpeed * Time.deltaTime);
+        }
+        else if (horz < 0 && this.transform.position.x > MinPosition)
+        {
+            this.transform.Translate(Vector3.right * horz * MoveSpeed * Time.deltaTime);
+        }
+
+        //control attacks
+        if (Input.GetKeyDown(KeyCode.LeftAlt) == true || Input.GetKeyDown(KeyCode.RightAlt) == true)
+        {
+            anim.SetTrigger("punch");
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftControl) == true || Input.GetKeyDown(KeyCode.RightControl) == true)
+        {
+            anim.SetTrigger("kick");
+        }
+    }
+    public void PunchOn()
+    {
+        punchbone.CanHit = true;
+    }
+    public void PunchOff()
+    {
+        punchbone.CanHit = false;
+    }
+    public void KickOn()
+    {
+        kickbone.CanHit = true;
+    }
+    public void KickOff()
+    {
+        kickbone.CanHit = false;
+    }
+    private HittingBodyPart GetBodyPartScript(string name)
+    {
+        //look for the child gameobject in the list of all the child objects
+        Transform[] children = this.GetComponentsInChildren<Transform>();
+        foreach (Transform child in children)
+        {
+            if (child.gameObject.name == name)
+            {
+                return child.gameObject.GetComponent<HittingBodyPart>();
+            }
+        }
+        return null;
+    }
+}
---
+++ HittingBodyPart.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HittingBodyPart : MonoBehaviour
+{
+    public string sndHit = "punch bag"; //the name of the audio file in resource folder
+    private bool _canHit = false;
+    private AudioSource audioHit = null;
+
+    //this is a property with ONLY a set
+    public bool CanHit
+    {
+        set
+        {
+            //when the caller sets the value we turn the Collider2D on/off appropriately
+            _canHit = value;
+            if (_canHit == true)
+            {
+                this.GetComponent<Collider2D>().enabled = true;
+            }
+            else
+            {
+                this.GetComponent<Collider2D>().enabled = false;
+            }
+        }
+    }
+    private void Start()
+    {
+        //make and reference the audio component (punch sound)
+        audioHit = CreateAudioSource(sndHit);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //play the punch sound
+        audioHit.Play();
+    }
+    private AudioSource CreateAudioSource(string filename)
+    {
+        //add an audio component to the gameobject
+        AudioSource audio = this.gameObject.AddComponent<AudioSource>();
+
+        //load a sound into the audio component
+        audio.clip = Resources.Load<AudioClip>(filename);
+
+        return audio;
+    }
+}
